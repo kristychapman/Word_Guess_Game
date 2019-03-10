6367c7decaec1b76ade0f85c00a28262d0fd48ea
kristychapman <kchapman1981@gmail.com>
2019-03-09 18:33:32 -0600

    some work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,13 @@
-var correct = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+$(document).ready(function() {
+    
+var letters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 
-if (correct) {
-  alert(petName + " " + petType + " " + petAge);
-}
-else {
-  alert("You don't get my cat's information");
-}
+letters.forEach(function(letter){
+  var letterBtn = $('<button>');
+  letterBtn.addClass('letter-button', 'letter', 'letter-button-color');
+  letterBtn.data('letter', letter);
+  letterBtn.text(letter);
+  $("#buttons").append(letterBtn);
+} 
+});
 
-if (petIsCat) {
-  alert("My pet is a cat");
-}
-else {
-  alert("My pet is not a cat");
-}
-
-if (petAge !== 7) {
-  alert("My pet is not 7 years old");
-}
-
-if (petAge === 5) {
-  alert("My pet is 5 years old");
-}
-else if (petAge < 5) {
-  alert("My pet is less than 5 years old");
-}
-else if (petAge > 5) {
-  alert("My pet is older than 5 years old");
-}
-
-// document.write() overwrites the entire page. We don't normally use this, but we will today for simplicity
-document.write("Welcome to our page " + userName);
-
-var catAge = prompt("How old is your cat?");
-
-// Won't work as expected
-alert(catAge + 2);
-
-// Works as expected
-alert(parseInt(catAge) + 2);
